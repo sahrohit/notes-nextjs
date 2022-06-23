14c8c5d26a0d38796769c40adac948e930f1873f
sahrohit <sahrohit9586@hmail.com>
2022-06-23 20:52:11 +0545

    Chnaged Database from SQLite to Postgres

--- config/data-source.ts
+++ config/data-source.ts
@@ -3,10 +3,14 @@ import { DataSource } from "typeorm";
 import { Note } from "@entities/Note";
 
 const AppDataSource = new DataSource({
-	type: "sqlite",
-	database: "database.sql",
-	entities: [Note],
+	type: "postgres",
+	url: process.env.DATABASE_URL,
 	synchronize: true,
+	logging: true,
+	entities: [Note],
+	migrations: ["./migrations/*.ts"],
+	migrationsTableName: "migrations",
+	subscribers: [],
 });
 
 export default AppDataSource;
